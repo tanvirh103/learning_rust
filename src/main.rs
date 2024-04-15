0eb955d7754c4b64700faafd799e2eda454864b4
tanvir103 <tanvirh103@gmail.com>
2024-04-15 18:02:49 +0600

    struct learned

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,17 @@ struct MyInfo{
     cgpa:f64,
     num:i64
 }
+struct Config{
+    port:u16
+}
 fn main() {
+    let config:Config=Config{port:8080};
+    let using:&Config=&config;
+    let mut con:Config=Config{port:1616};
+    let s:&mut Config=&mut con;
+    s.port=4000;
+    println!("{}",using.port);
+    println!("changed port: {}",con.port);
     let m=My(true,-5,5);
     let l=My(false,10,5);
     println!("{:?}",l);
